cb465190f878c1a8947500f15746aca505a1950d
effilin <effienummer@gmail.com>
2024-10-16 12:23:10 -0400

    data types

--- Display.java
+++ Display.java
@@ -2,4 +2,10 @@ public class Display {
     public static void main(String[]args) {
         System.out.println("Java X");
     }
+    public static void main(Integer[]args) {
+        int age = 34;
+        System.out.println(age);
+    }
+
 }
+
