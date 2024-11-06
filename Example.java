28d5a9c2603b9aebaaaaeb651688fff72bc6fe2f
effilin <effienummer@gmail.com>
2024-11-06 15:41:39 -0500

    workign on conditional statements in Java

---
+++ Example.java
@@ -0,0 +1,15 @@
+public class Example {
+    public static void main(String[] args) {
+        String light = "green";
+
+        if( light == "green") {
+            System.out.println("please keep going");
+        }else if (light == "yellow") {
+            System.out.println("please go slow");
+        } else {
+            System.out.println("please stop");
+        }
+
+    }
+    
+}
